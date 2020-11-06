da589f0ad6fb54f64f2e64ce5b2ab14be73d672c
ralphieraccoon <damian.crosby@manchester.ac.uk>
2020-11-06 14:35:28 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 for val in ${INPUT_SOURCE_FILE//,/ };
 do
-  cp -R $val "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$(dirname $val)"  
+  cp --parents -R $val "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$(dirname $val)"  
 done
 cd "$CLONE_DIR"
 
